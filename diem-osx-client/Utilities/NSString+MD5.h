4a2049184662584cdab6947c45f5935d82b99b22
Esben Sorig <eas@trifork.com>
2014-02-01 19:16:18 +0100

    Now MD5 hashing diem directory path

---
+++ diem-osx-client/Utilities/NSString+MD5.h
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.h
+//  diem-osx-client
+//
+//  Created by Esben Sorig on 01/02/2014.
+//  Copyright (c) 2014 Esben Sorig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+@end
---
+++ diem-osx-client/Utilities/NSString+MD5.m
@@ -0,0 +1,13 @@
+//
+//  NSString+MD5.m
+//  diem-osx-client
+//
+//  Created by Esben Sorig on 01/02/2014.
+//  Copyright (c) 2014 Esben Sorig. All rights reserved.
+//
+
+#import "NSString+MD5.h"
+
+@implementation NSString (MD5)
+
+@end
